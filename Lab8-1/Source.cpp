d978fe1a53d66a94e8d4085150aad213b57bd360
Micah Lee <m-lee.1@onu.edu>
2024-10-24 08:23:55 -0400

    Completed countCharacter function

--- Lab8-1/Source.cpp
+++ Lab8-1/Source.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int countCharacter(string str);
+
+int main(void) {
+	string str = "tooearly";
+	cout << countCharacter(str);
+}
+
+int countCharacter(string str) {
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++) {
+		count++;
+	}
+
+	return count;
+}
+
