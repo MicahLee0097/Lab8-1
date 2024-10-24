0eeb4a2321d06733a6bc86f6e58d480b4087e8ed
Micah Lee <m-lee.1@onu.edu>
2024-10-24 08:41:07 -0400

    Completed Lab8-1

--- Lab8-1/Source.cpp
+++ Lab8-1/Source.cpp
@@ -1,15 +1,31 @@
+/*
+Micah Lee
+Lab 8-1
+Oct 24. 2024
+*/
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+// function prototype
 int countVowel(string str);
 
+// main function, takes input string and calls countVowel function
 int main(void) {
-	string str = "tOOearly";
-	cout << countVowel(str);
-}
+	string input;
 
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+
+		if (input == "Q") { break; }
+
+		cout << "Vowel count: " << countVowel(input) << endl;
+	}
+}
+ // takes string and counts upercase and lowercase vowels, return count
 int countVowel(string str) {
 	int i, count = 0;
 
