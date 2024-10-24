e4ebf2884d2d1ea567de17a038148413ad0ab50c
Micah Lee <m-lee.1@onu.edu>
2024-10-24 08:33:16 -0400

    Completed countVowel function

--- Lab8-1/Source.cpp
+++ Lab8-1/Source.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
 
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main(void) {
-	string str = "tooearly";
-	cout << countCharacter(str);
+	string str = "tOOearly";
+	cout << countVowel(str);
 }
 
-int countCharacter(string str) {
+int countVowel(string str) {
 	int i, count = 0;
 
 	for (i = 0; str[i] != '\0'; i++) {
-		count++;
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+			count++;
+		}
+		else if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+			count++;
+		}
+		else {}
 	}
 
 	return count;
